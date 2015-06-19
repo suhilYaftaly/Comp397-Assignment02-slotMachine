854fe8c3aaaac75ae22bff7636ccab8bf2409196
suhilYaftaly <suhail278@gmail.com>
2015-06-19 12:50:09 -0400

    added slot machine assets

--- COMP397-Ass02-slotMachine/Scripts/game.js
+++ COMP397-Ass02-slotMachine/Scripts/game.js
@@ -1,84 +1,64 @@
-﻿/// <reference path="typings/stats/stats.d.ts" />
+/// <reference path="typings/stats/stats.d.ts" />
 /// <reference path="typings/easeljs/easeljs.d.ts" />
 /// <reference path="typings/tweenjs/tweenjs.d.ts" />
 /// <reference path="typings/soundjs/soundjs.d.ts" />
 /// <reference path="typings/preloadjs/preloadjs.d.ts" />
-
-
 // Game Framework Variables
 var canvas = document.getElementById("canvas");
-var stage: createjs.Stage;
-var stats: Stats;
-
-var assets: createjs.LoadQueue;
+var stage;
+var stats;
+var assets;
 var manifest = [
     { id: "background", src: "assets/images/slotMachine.jpg" },
     { id: "clicked", src: "assets/audio/clicked.wav" }
 ];
-
-
 // Game Variables
-var background: createjs.Bitmap;
-
-
+var background;
 // Preloader Function
 function preload() {
     assets = new createjs.LoadQueue();
     assets.installPlugin(createjs.Sound);
     // event listener triggers when assets are completely loaded
-    assets.on("complete", init, this); 
+    assets.on("complete", init, this);
     assets.loadManifest(manifest);
     //Setup statistics object
     setupStats();
 }
-
 // Callback function that initializes game objects
 function init() {
     stage = new createjs.Stage(canvas); // reference to the stage
     stage.enableMouseOver(20);
     createjs.Ticker.setFPS(60); // framerate 60 fps for the game
     // event listener triggers 60 times every second
-    createjs.Ticker.on("tick", gameLoop); 
-
+    createjs.Ticker.on("tick", gameLoop);
     // calling main game function
     main();
 }
-
 // function to setup stat counting
 function setupStats() {
     stats = new Stats();
     stats.setMode(0); // set to fps
-
     // align bottom-right
     stats.domElement.style.position = 'absolute';
     stats.domElement.style.left = '330px';
     stats.domElement.style.top = '10px';
-
     document.body.appendChild(stats.domElement);
 }
-
-
 // Callback function that creates our Main Game Loop - refreshed 60 fps
 function gameLoop() {
     stats.begin(); // Begin measuring
-
     stage.update();
-
     stats.end(); // end measuring
 }
-
 // Callback function that allows me to respond to button click events
-function pinkButtonClicked(event: createjs.MouseEvent) {
+function pinkButtonClicked(event) {
     createjs.Sound.play("clicked");
 }
-
 // Callback functions that change the alpha transparency of the button
-
-
-
 // Our Main Game Function
 function main() {
-   // add in slot machine graphics
+    // add in slot machine graphics
     background = new createjs.Bitmap(assets.getResult("background"));
-    stage.addChild(background); 
+    stage.addChild(background);
 }
+//# sourceMappingURL=game.js.map
